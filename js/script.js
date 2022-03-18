efaba5156dfb9abfb33e229e7c96ad76bb7eaa0d
Tomo-K-R <tomomikojo@hotmail.com>
2022-03-18 18:19:33 +0000

    added click event listner and other functions to randomly select index of image, display image and its author, and display

--- js/script.js
+++ js/script.js
@@ -7,5 +7,33 @@ const getImage = async function(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    
+    selectRandomImages(images);
 };
-getImage();
+
+// Randomly Select the Index of an Image
+const selectRandomImages = function(images){
+    const randomIndex = Math.floor(Math.random()*images.length);
+    const randomImage = images[randomIndex];
+    
+    displayImage(randomImage);
+    // console.log(randomIndex);
+    // console.log(randomImage);
+};
+
+// Display the Image
+const displayImage = function(randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+// Added a Click Event
+button.addEventListener("click", function(){
+    getImage();
+});
+
+
